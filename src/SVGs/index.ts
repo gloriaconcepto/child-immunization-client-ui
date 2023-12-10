8722e73db4307d836633b04f9774501b941316c8
gloriaconcepto <gloriaconcepto@outlook.com>
2023-12-10 16:23:26 +0100

    baby img center added

--- src/SVGs/index.ts
+++ src/SVGs/index.ts
@@ -1,7 +1,9 @@
 import BabyLeft from './baby-left.svg';
 import BabyRight from './baby-right.svg';
+import BabyCenter from './baby-center.svg';
 const Icon = {
     BabyLeft,
-    BabyRight
+    BabyRight,
+    BabyCenter
 }
 export default Icon
--- src/components/formCard/index.tsx
+++ src/components/formCard/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import './formcontainer.scss'
+import Icon from "../../SVGs";
 const FormContainer: React.FC = () => {
     return (
       <div className="formcontainer">
-
+        <div className="img-center-wrapper">
+        <img src={Icon.BabyCenter} alt="baby center" className="img-center"/> 
+          </div>
+          
       </div>
     );
   };
-  
+
   export default FormContainer;
   
